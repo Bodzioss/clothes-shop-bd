7c5f56c3709c63807f69331df410a904f85ad128
Bodzioss <marcin_bogdanski1@wp.pl>
2022-01-30 15:44:56 +0100

    Final Update

--- Clothes_Shop/Clothes_Shop/Models/BD2SklepContext.cs
+++ Clothes_Shop/Clothes_Shop/Models/BD2SklepContext.cs
@@ -44,8 +44,7 @@ namespace Clothes_Shop.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-AMGTN3U;Database=BD2.Sklep2;Trusted_Connection=True;");
+
             }
         }
 
